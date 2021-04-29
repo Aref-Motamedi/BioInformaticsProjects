13b69275047323dffaf9ecfbc5f23888c9fc9414
Aref Motamedi <aref78.m@gmail.com>
2021-04-30 00:22:45 +0430

    created 2 distance matrix. have got into some problems with it. SO i created a REAL Tree class for it. now all distance matrixes use tree and leafs

--- HW2.py
+++ HW2.py
@@ -1,4 +1,16 @@
 import numpy
+import math
+
+class Node:
+    # def __init__(self,sequense):
+    #     self.sequense=sequense
+    #     self.leftchild=None
+    #     self.rightchild=None
+
+    def __init__(self,sequense,leftchild,rightchild):
+        self.sequense=sequense 
+        self.leftchild=leftchild
+        self.rightchild=rightchild
 
 def global_align(x, y, s_match, s_mismatch, s_gap):
     A = []
@@ -45,6 +57,67 @@ def global_align(x, y, s_match, s_mismatch, s_gap):
             j = j - 1
     return (align_X, align_Y,A[len(y)][len(x)])
 
+def findconsensus(sequenses):
+    consensus =""
+    for char in range(len(sequenses[0])):
+        charlist = []
+        for s in sequenses:
+            if (s[char] == '-'):
+                charlist.append('}}')
+            else:
+                charlist.append(s[char])
+        minchar = min(charlist)
+        if (minchar=='}}'):
+            minchar = '-'
+        consensus += min(charlist)
+    
+    return consensus
+
+def makeDistanceMatrix(leafs):
+    distanceMatrix =[[0 for c in range(len(leafs))] for r in range(len(leafs))]
+    distanceMatrixMax = [0 for c in range(len(leafs))]
+    for i in range(len(leafs)):
+        for j in range(len(leafs)):
+            if (i==j):
+                distanceMatrix[i][j] =None
+                distanceMatrixMax[i] +=0
+            elif (distanceMatrix[j][i] == 0):
+                distanceMatrix[i][j] = global_align(leafs[i].sequense,leafs[j].sequense,1,-1,-2)[2]
+                distanceMatrixMax[i] +=distanceMatrix[i][j]
+            else:
+                distanceMatrix[i][j] = distanceMatrix[j][i]
+                distanceMatrixMax[i] +=distanceMatrix[i][j]
+
+    return distanceMatrix,distanceMatrixMax
+
+def makeNewDM (leafs,distanceMatrix,distanceMatrixMax):
+    
+    newdistancematrix = [[0 for c in range(len(leafs))] for r in range(len(leafs))]
+    mininnew = [[999999999,None,None]]
+    for i in range(len(leafs)):
+        for j in range(len(leafs)):
+            if (i!=j):
+                newdistancematrix[i][j] = distanceMatrix[i][j] - ((distanceMatrixMax[i] + distanceMatrixMax[j])/(len(leafs)-2))
+                if (mininnew[0][0]>newdistancematrix[i][j]):
+                    mininnew=[[newdistancematrix[i][j],i,j]]
+                elif (mininnew[0][0] == newdistancematrix[i][j]):
+                    mininnew.append([newdistancematrix[i][j],i,j])
+    
+    print(mininnew)
+    if(len(mininnew) >1):
+        temp=mininnew[0]
+        minnum=min(temp[1],temp[2])
+        for elem in mininnew:
+            if (elem[1]<minnum) or (elem[2]<minnum):
+                temp = elem
+                minnum=min(temp[1],temp[2])
+        
+        mininnew=temp
+
+    return newdistancematrix,mininnew
+
+
+
 def __main__():
     
     # print (global_align('HVLIP','HVLP',1,-1,-2))
@@ -58,25 +131,70 @@ def __main__():
     # for i in range(seqnumber) :
     #     sequenses.append(input())
     # print (sequenses)
+    leafs = []
+    for seq in sequenses:
+        leafs.append( Node(seq,None,None) )
+    # for leaf in leafs:
+    #     print(leaf.sequense)
+    # print(len(newnodes))
+    while seqnumber>1:
+    # while len(leafs)>1:
+        # seqnumber=2
+        
+        # distanceMatrix =[[0 for c in range(seqnumber)] for r in range(seqnumber)]
+        # distanceMatrixMax = [0 for c in range(seqnumber)]
+        # for i in range(seqnumber):
+        #     for j in range(seqnumber):
+        #         if (i==j):
+        #             distanceMatrix[i][j] =None
+        #             distanceMatrixMax[i] +=0
+        #         elif (distanceMatrix[j][i] == 0):
+        #             distanceMatrix[i][j] = global_align(sequenses[i],sequenses[j],1,-1,-2)[2]
+        #             distanceMatrixMax[i] +=distanceMatrix[i][j]
+        #         else:
+        #             distanceMatrix[i][j] = distanceMatrix[j][i]
+        #             distanceMatrixMax[i] +=distanceMatrix[i][j]
 
-    distanceMatrix =[[0 for c in range(seqnumber)] for r in range(seqnumber)]
-    # distanceMatrixMax = [0 for c in range(seqnumber)]
-    for i in range(seqnumber):
-        for j in range(seqnumber):
-            if (i==j):
-                distanceMatrix[i][j] =None
-                # distanceMatrixMax[i] +=0
-            elif (distanceMatrix[j][i] == 0):
-                distanceMatrix[i][j] = global_align(sequenses[i],sequenses[j],1,-1,-2)[2]
-                # distanceMatrixMax[i] +=distanceMatrix[i][j]
-            else:
-                distanceMatrix[i][j] = distanceMatrix[j][i]
-                # distanceMatrixMax[i] +=distanceMatrix[i][j]
+        distanceMatrix,distanceMatrixMax = makeDistanceMatrix(leafs)
+                
+        print(distanceMatrix)
+        print(distanceMatrixMax)
 
-            
+        # newdistancematrix = [[0 for c in range(seqnumber)] for r in range(seqnumber)]
+        # mininnew = [999999999,None,None]
+        # for i in range(seqnumber):
+        #     for j in range(seqnumber):
+        #         if (i!=j):
+        #             newdistancematrix[i][j] = distanceMatrix[i][j] - ((distanceMatrixMax[i] + distanceMatrixMax[j])/(seqnumber-2))
+        #             if (mininnew[0]>newdistancematrix[i][j]):
+        #                 mininnew=[newdistancematrix[i][j],i,j]
+        newdistancematrix,mininnew = makeNewDM(leafs,distanceMatrix,distanceMatrixMax)
 
-    print(distanceMatrix)
-    # print(distanceMatrixMax)
+        print (newdistancematrix)
+        print (mininnew)
+        
+        newalignment = global_align(sequenses[mininnew[1]],sequenses[mininnew[2]],1,-1,-2)
+        # print(sequenses[1][3]) 
+
+        print (findconsensus(['abcd','-avf','jd-e']))
+
+
+        seqnumber =1
+
+    
+    
+    # distanceMatrix1 = distanceMatrix
+    # guidetree = []
+    # for c in range(math.ceil(seqnumber/2)):
+    #     minInDistanceM = [99999999,0,0]
+    #     for i in range(seqnumber):
+    #         for j in range(seqnumber):
+    #             if ((i!=j) and distanceMatrix1[i][j]<minInDistanceM[0] and distanceMatrix1[i][j]!=None):
+    #                 minInDistanceM = [distanceMatrix1[i][j],i,j]
+    #     # dis
+    #     guidetree.append(minInDistanceM[1])
+    #     guidetree.append(minInDistanceM[2])
+
 
 
 
