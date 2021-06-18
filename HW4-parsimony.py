e5f28b563be62fea879777ed6a854c350353ce71
Aref Motamedi <aref78.m@gmail.com>
2021-06-19 00:18:45 +0430

    hw4-parsimony is written but the answer is not good

--- HW4-parsimony.py
+++ HW4-parsimony.py
@@ -17,11 +17,17 @@ class Node:
 
 
 def returnAparent(nodes,tag):
+    
+    # for nod in nodes:
+    #     print(tag[nodes.index(nod)])
+
     for nod in nodes:
-        if (not nod.leaf):
+        if (tag[nodes.index(nod)] ==0):
+            # print(nod.leaf)
+            
             flag = True
             for child in nod.children:
-                if tag[nodes.index(child)] ==1:
+                if tag[nodes.index(child)] ==0:
                     flag=False
             if flag==True:
                 return nod
@@ -47,17 +53,23 @@ def smallParsimony(nodes,collumn):
     while (not flag):
         nod = returnAparent(nodes,tag)
         if (nod==None):
-            flag = False
+            flag = True
         else:
             tag[nodes.index(nod)] = 1
             for alpha in alphabet:
-                print("k")
+                minSon =[99999999,None]
+                for child in nod.children:
+                    minSVson = min(sv[nodes.index(child)].items(), key=lambda x: x[1])
+                    price=0
+                    if alpha != minSVson[0] : 
+                        price = 1
+                    if minSon[0] > minSVson[1]+price:
+                        minSon = [minSVson[1]+price,minSVson[0]]
+                sv[nodes.index(nod)][alpha] = minSon[0]
+    
+    return min(sv[0].items(), key=lambda x: x[1])
+
             
-
-
-
-
-
     
 def __main__():
 
@@ -70,13 +82,14 @@ def __main__():
         nodes[int(inp[0])].addChild(nodes[int(inp[1])])
     
     numberOfLeaves = int(input())
+    seqLength = None
     for i in range(numberOfLeaves):
         inp = input().split(" ")
         nodes[int(inp[0])].addSeq(inp[1])
+        seqLength = len(inp[1])    
     
-    # for nod in nodes:
-    #     print(nodes.index(nod),nod.children,nod.sequence)
-
+    for i in range(seqLength):
+        print(smallParsimony(nodes,i))
 
 
 
